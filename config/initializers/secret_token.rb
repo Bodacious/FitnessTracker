250fd9587929f56fd4b2d19ad603d534eba0ba91
Bodacious <gavin@katanacode.com>
2012-05-10 23:31:05 +0100

    Added unicorn.rb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FitnessTracker::Application.config.secret_token = '29c161dd8e2645ba099fd138684f8b70ca6a8622ee0d6fe1fabd48cee3bda957bb128fac14a4f8d9e5c0008af390feec8a7267c5ac10745d30edf9814963fb3a'
